5f97d7ee2acda251104599f7731fe6578db95004
adk-saugat <adhikarisaugat34@gmail.com>
2025-08-22 12:13:04 -0500

    basic version

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main(){
-	
+	prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.1, 0.15}
+
+	result := make(map[float64][]float64)
+
+	for _, taxValue := range taxRates{
+		taxIncludedPrices := make([]float64, len(prices))
+		for priceIndex, priceValue := range prices{
+			taxIncludedPrices[priceIndex] = priceValue * (1 + taxValue)
+		}
+		result[taxValue] = taxIncludedPrices
+	}
+	fmt.Println(result)
 }
